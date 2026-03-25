abdc672368818cf9ccc3aebea58391ef899b66a1
Donal Mac An Ri <donal@bluedrop.com>
2026-03-25 09:29:19 -0230

    feat: add divide function, bump to v1.2.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Sample application file
 // This file exists to demonstrate the merge and release workflow
 
-const APP_VERSION = '1.1.0';
+const APP_VERSION = '1.2.0';
 
 function greet(name) {
     return `Hello, ${name}! Welcome to version ${APP_VERSION}`;
@@ -19,4 +19,9 @@ function multiply(a, b) {
     return a * b;
 }
 
-module.exports = { greet, add, subtract, multiply, APP_VERSION };
+function divide(a, b) {
+    if (b === 0) throw new Error('Division by zero');
+    return a / b;
+}
+
+module.exports = { greet, add, subtract, multiply, divide, APP_VERSION };
